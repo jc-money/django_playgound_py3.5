0bdf0f00c341594b25a739bb19cf6df7781b8d45
Jamie Czuy <jamie.czuy@turner.com>
2016-05-13 17:45:24 -0400

    Add healthcheck

--- playground/playground/urls.py
+++ playground/playground/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.http import HttpResponse
+
+
+def healthcheck(request):
+    return HttpResponse('OK')
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^_healthcheck$', healthcheck),
 ]
